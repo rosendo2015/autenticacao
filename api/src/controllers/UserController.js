1194e4f7ac19b072b513fe6fd3a1ef0a9ccfc31b
rosendo <rosendc30@gmail.com>
2023-12-26 20:26:27 -0300

    update dos dados de usuário

--- api/src/controllers/UserController.js
+++ api/src/controllers/UserController.js
@@ -18,5 +18,30 @@ class UserController {
 
     response.status(201).json()
   }
+  async update(request, response) {
+    const { name, email } = request.body;
+    const { id } = request.params;
+
+    const database = await sqliteConnect()
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id])
+    if (!user) {
+      throw new AppError("Usuário não encontrado.")
+    }
+    const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+    if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+      throw new AppError("Este e-mail já está em uso.")
+    }
+    user.name = name ?? user.name
+    user.email = email ?? user.email
+
+    await database.run(`
+    UPDATE users SET
+    name = ?,
+    email = ?,
+    updated_at = DATETIME('now')
+    WHERE id = ?`, [user.name, user.email, id]);
+
+    return response.status(200).json();
+  }
 }
 module.exports = UserController;
