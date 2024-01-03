e7f0f1e59525ec89727f9efd85caf36185a3b5d8
rosendo <rosendc30@gmail.com>
2024-01-02 22:19:42 -0300

    Profiler

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,9 +1,9 @@
-const { Router } = require("express")
-const usersRoutes = require("./users.routes")
-const sessionsRouter = require("./sessions.rutes")
-const routes = Router()
+const { Router } = require("express");
+const usersRoutes = require("./users.routes");
+const sessionsRouter = require("./sessions.rutes");
+const routes = Router();
 
-routes.use("/users", usersRoutes)
-routes.use("/sessions", sessionsRouter)
+routes.use("/users", usersRoutes);
+routes.use("/sessions", sessionsRouter);
 
-module.exports = routes
+module.exports = routes;
---
+++ web/src/pages/Profiler/index.jsx
@@ -0,0 +1,10 @@
+const { Container } = require("./styled");
+
+function Profiler() {
+  return (
+    <Container>
+      <img src="https://github.com/rosendo2015.png" alt="photo user" />
+      <h1>profiler</h1>
+    </Container>
+  )
+}
